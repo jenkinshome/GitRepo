048a305db08f70515b7eb7c24d3b0a408054228d
jenkinshome <101237605+jenkinshome@users.noreply.github.com>
2022-04-16 09:42:59 +0530

    Update Organization.java

--- Organization.java
+++ Organization.java
@@ -4,6 +4,12 @@ class Organization{
    System.out.println("  Organization ");
    System.out.println(" * * * * * ");
    System.out.println(" Master Branch ");
+   System.out.println(" Forking Repository From Jenkinshome1 To Jenkinsg ");
+   System.out.println(" ----------- ");
+   System.out.println(" ----------- ");
+   System.out.println(" Organization Branch ");
+   System.out.println(" Forking Repository From Jenkinshome1 To Jenkinshome ");
+   System.out.println("_________________ ");
   }
 }
 
