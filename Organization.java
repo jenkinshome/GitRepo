1981b34dcdb6114bb87b6149b9993b5d1dc5fe6a
jenkinsg <103235835+jenkinsg@users.noreply.github.com>
2022-04-16 09:26:09 +0530

    Update Organization.java

--- Organization.java
+++ Organization.java
@@ -2,6 +2,8 @@ class Organization{
  public static void main(String args[]) {
    System.out.println(" *  *  *");
    System.out.println("  Organization ");
+   System.out.println(" * * * * * ");
+   System.out.println(" Master Branch ");
   }
 }
 
